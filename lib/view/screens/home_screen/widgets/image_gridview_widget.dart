634ea503e4555c52228dab8a4d5c73e45dbde250
Aslamabdulla <themanwithgunpower@gmail.com>
2022-11-08 06:14:29 +0530

    image_preview_completed

--- lib/view/screens/home_screen/widgets/image_gridview_widget.dart
+++ lib/view/screens/home_screen/widgets/image_gridview_widget.dart
@@ -1,61 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
+
 import 'package:pixabay_search_sample/controller/home_screen_controller.dart';
 import 'package:pixabay_search_sample/services/services.dart';
-import 'package:pixabay_search_sample/view/common/common.dart';
 
-// class ImageGridviewWidget extends StatelessWidget {
-//   const ImageGridviewWidget({
-//     Key? key,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     double height = Get.height;
-//     double width = Get.width;
-//     return Container(
-//       margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-//       height: height * .87,
-//       width: width,
-//       child: GridView.custom(
-//         physics: const BouncingScrollPhysics(),
-//         primary: false,
-//         gridDelegate: SliverWovenGridDelegate.count(
-//           crossAxisCount: 2,
-//           mainAxisSpacing: 0,
-//           crossAxisSpacing: 10,
-//           pattern: [
-//             const WovenGridTile(1),
-//             const WovenGridTile(
-//               5 / 7,
-//               crossAxisRatio: 1,
-//               alignment: AlignmentDirectional.centerEnd,
-//             ),
-//           ],
-//         ),
-//         childrenDelegate: SliverChildBuilderDelegate(
-//           addSemanticIndexes: false,
-//           childCount: 10,
-//           (context, index) {
-//             return Container(
-//               height: 100,
-//               width: 100,
-//               decoration: gridTileDecoration,
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:pixabay_search_sample/view/screens/home_screen/widgets/image_preview_widget.dart';
 
 class ImageGridviewWidget extends StatelessWidget {
   const ImageGridviewWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var height = Get.height;
+    var width = Get.width;
     ImageDatabase database = ImageDatabase();
     return GetBuilder<HomeController>(builder: (homeCtrl) {
       return homeCtrl.isLoading.value == false
@@ -73,12 +33,23 @@ class ImageGridviewWidget extends StatelessWidget {
                   final imagesNetwork =
                       homeCtrl.pixabayImages[index].previewURL;
 
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.network(
-                      imagesNetwork,
-                      filterQuality: FilterQuality.low,
-                      fit: BoxFit.cover,
+                  return GestureDetector(
+                    onTap: () {
+                      Get.defaultDialog(
+                        textCancel: "OK",
+                        title: "PIXABAY IMAGE",
+                        content:
+                            ShowImageWidget(homeCtrl: homeCtrl, index: index),
+                        onCancel: () {},
+                      );
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(
+                        imagesNetwork,
+                        filterQuality: FilterQuality.low,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   );
                 } else {
